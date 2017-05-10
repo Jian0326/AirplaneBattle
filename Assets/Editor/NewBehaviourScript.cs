64f68cca3ba973343568a35e07b4c27f552e0245
Jerry <jian820326@gmail.com>
2017-05-10 15:04:39 +0800

    修改文字位置显示错乱

---
+++ Assets/Editor/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CustomEditor(typeof(NewBehaviourScript))]
+public class NewBehaviourScript : UnityEditor.AssetModificationProcessor {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    static public void OnWillSaveAssets(string[] names)
+    {
+
+        foreach (string name in names)
+        {
+            if (name.EndsWith(".unity"))
+            {
+                Scene scene = SceneManager.GetSceneByPath(name);
+                Debug.Log("雨松MOMO提醒您保存的场景名称是 ：" + scene.name);
+            }
+
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
